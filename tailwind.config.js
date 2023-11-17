ad974edf2df4c359bc7fa7536239dc055447674e
nn-fiha <jucse28.361@gmail.com>
2023-11-17 23:48:36 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      'xs': '400px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+},
+    extend: {
+      colors:{
+        'clrbg':'#f0f1f2',
+        'purple':'#86608E',
+        'hvr-purple' : '#57375D',
+        'purp':'#461959',
+        'pastel':'#b4d9ef',
+      
+      },
+
+      //  #8d818c 
+      height:{
+        'lgcover':'650px',
+        'smcover':'500px',
+      }
+    },
+  },
+  plugins: [],
+}
+
